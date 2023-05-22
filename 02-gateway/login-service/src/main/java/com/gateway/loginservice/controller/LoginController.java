ff5c2c9cb69c9b6fea395326b6e2e3d73a31fb6f
zongyanan <yanan_zong@huace.cn>
2023-05-22 10:19:03 +0800

    Initial commit

---
+++ 02-gateway/login-service/src/main/java/com/gateway/loginservice/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.gateway.loginservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+public class LoginController {
+
+    @GetMapping("login")
+    public String login(String name,String pwd){
+        System.out.println(name+pwd);
+        String token = UUID.randomUUID().toString();
+        return token;
+    }
+}
---
+++ 02-gateway/login-service/src/test/java/com/gateway/loginservice/LoginServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gateway.loginservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
